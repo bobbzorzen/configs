173db71e9c2715ef888729786ee56c91c3c9e35a
Leopold Olsson <bobbzorzen@gmail.com>
2017-09-22 20:36:48 +0000

    added git init things to setup script

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ sudo apt-get install git weechat-curses tmux
 
 cp .bashrc ~/
 source ~/.bashrc
+
+
+git config --global user.email "bobbzorzen@gmail.com"
+git config --global user.name "Leopold Olsson"
+git config --global user.username "bobbzorzen"
